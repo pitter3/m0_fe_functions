cce8b284efe2e4f52977fd5897e92296221301cd
Patrick Eitel <patrickeitel12@gmail.com>
2023-06-08 23:25:02 -0700

    Add answers to functions.js. Add reflection log

--- functions.js
+++ functions.js
@@ -1,24 +1,63 @@
 // Defining Our Own Functions
-// For each exercise below, write the function according to the requirements. 
-// Call each method at least twice and store the return value in a variable. 
-// Use console.log() to see the return value in the console.
+    // For each exercise below, write the function according to the requirements. 
+    // Call each method at least twice and store the return value in a variable. 
+    // Use console.log() to see the return value in the console.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
 
+function greeting() {
+    return "Hello friend!";
+}
+//I know that 2 variables is redundant here, but the directions say to do this!
+var greeting1 = greeting();
+var greeting2 = greeting();
+console.log(greeting());
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
+
+function customGreeting(name) {
+    return `Hello ${name}.`;
+}
+
+var callOne = customGreeting("Patrick");
+var callTwo = customGreeting("Nicole");
+console.log(callOne, callTwo);
 
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
+function greetPerson(first, middle, last) {
+    return `${first} ${middle} ${last} is an awesome person.`;
+}
+
+var greetingA = greetPerson("Patrick", "Hall", "Eitel");
+var greetingB = greetPerson("Erin", "Nicole", "Eitel");
+console.log(greetingA);
+console.log(greetingB);
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
 
+function square(num) {
+    return num * num;
+}
+var squareOfFive = square(5);
+var squareOfTwelve = square(12);
+console.log(`5 squared is ${squareOfFive}`);
+console.log(`12 squared is ${squareOfTwelve}`);
+//HOLY SHIT IT WORKED.
 
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
-
+function checkStock(amount, item) {
+   if (amount >= 4) { 
+   console.log(`${item} is stocked`);
+   } else if (amount > 0) {
+    console.log(`${item} is running LOW`);
+    } else { 
+        console.log(`${item} OUT of stock!`);
+     }
+    }
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
 
@@ -30,3 +69,5 @@ checkStock(0, "Cheese");
 
 checkStock(1, "Salsa");
 // => "Salsa - running LOW"
+
+//VICTORY!!!!!
--- selfEval.md
+++ selfEval.md
@@ -1,4 +1,9 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+    I am most proud of the amount of effort and time I put in today. While my assingments weren't in at the official due date, I really took the time needed to understand the concepts and worked extremely dilligently. I collaborated a ton with Nicole and Kasandra today as well, I am feeling really happy about my understanding of the fundamentals of this language.
+
 - In your own words, why are functions a powerful tool in our applications?
+    Functions are extremely powerful because you can make them do just about anything you want them to do (if you have to knowledge to code them). The last example with the fruit sorting made me realize that combining multiple concepts together can unlock a ton of power in just a few lines of code. 
+
+    It is 11:22 PM now for me. I am just doing my reflections now and pushing these to GitHub after a very long day of coding, collaborating, learning, and struggling, but almost all of my struggle today was productive.
